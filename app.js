bb2963606dc3044f5c9858fec3fde39627be96c9
yalcinagirk <56399240+yalcinagirk@users.noreply.github.com>
2020-04-30 17:40:24 +0300

    updated

--- app.js
+++ app.js
@@ -1,69 +1,34 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const redisStore = require('./helpers/redisStore');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const dotenv = require('dotenv');
-dotenv.config();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const index = require('./routes/index');
-const auth = require('./routes/auth');
-const chat = require('./routes/chat');
-const messages = require('./routes/messages');
-
-const app = express();
-
-// helpers
-const db = require('./helpers/db')();
-
-// middlewares
-const isAuthenticated = require('./middleware/isAuthenticated');
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'bower_components')));
 
-// express-session
-app.use(session({
-	store: redisStore,
-	secret: process.env.SESSION_SECRET_KEY,
-	resave: false,
-	saveUninitialized: true,
-	cookie: { maxAge: 14 * 24 * 3600000  }
-}));
-
-// passport.js
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/', index);
-app.use('/auth', auth);
-app.use('/chat', isAuthenticated, chat);
-app.use('/messages', isAuthenticated, messages);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- middleware/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-function isAuthenticated(req, res, next){
-	if (req.isAuthenticated())
-		next();
-	else
-		res.redirect('/');
-}
-
-module.exports = isAuthenticated;
